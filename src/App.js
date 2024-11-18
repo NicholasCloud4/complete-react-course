0a364828e28ee004ed76b6c77785fd26d0fadf1d
Nicholas <91276870+NicholasCloud4@users.noreply.github.com>
2024-11-18 16:10:49 -0500

    Applied CSS to Component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
+import "./App.css"
+
 function App() {
 
     let msg = "awesome"
 
     return (
         <>
-            <h1>This is my header</h1>
-            <p>This is my first {msg} React app {10 + 8}</p>
+            <h1 className="myHeader">This is my header</h1>
+            <p className="myParagraph">This is my first {msg} React app {10 + 8}</p>
             <input type="text"></input>
         </>
     )
