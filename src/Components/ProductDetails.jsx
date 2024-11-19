df4285a2d99ae5ca33b65b2fa42ddea99fcae2a8
Nicholas <91276870+NicholasCloud4@users.noreply.github.com>
2024-11-19 11:58:42 -0500

    A closer look at JSX Syntax

--- src/Components/ProductDetails.jsx
+++ src/Components/ProductDetails.jsx
@@ -11,15 +11,23 @@ function ProductDetails(props) {
     let badgeClass = "badge-margin-left-240 badge ";
     badgeClass += props.isAvailable ? "badge bg-success" : "badge bg-danger"
 
-    return (
-        <div className="d-flex align-items-center justify-content-start mt-1">
-            <h6 className="font-weight-bold my-2" style={{ marginRight: "30px" }}>${props.price}</h6>
-            <Button>-</Button>
-            <span style={{ padding: "0px 20px", fontSize: "12px" }}>{displayFormattedProductCount()}</span>
-            <Button>+</Button>
-            <span className={badgeClass}>{props.isAvailable ? "Available" : "Unavailable"}</span>
+    // return (
+    //     <div className="d-flex align-items-center justify-content-start mt-1">
+    //         <h6 className="font-weight-bold my-2" style={{ marginRight: "30px" }}>${props.price}</h6>
+    //         <Button>-</Button>
+    //         <span style={{ padding: "0px 20px", fontSize: "12px" }}>{displayFormattedProductCount()}</span>
+    //         <Button>+</Button>
+    //         <span className={badgeClass}>{props.isAvailable ? "Available" : "Unavailable"}</span>
 
-        </div>
+    //     </div>
+    // )
+
+    return React.createElement("div", { className: "d-flex align-items-center justify-content-start mt-1" },
+        React.createElement("h6", { className: "font-weight-bold my-2", style: { marginRight: "30px" } }, "$" + props.price),
+        React.createElement(Button, {}, "-"),
+        React.createElement("span", { style: { padding: "0px 20px", fontSize: "12px" } }, displayFormattedProductCount()),
+        React.createElement(Button, {}, "+"),
+        React.createElement("span", { className: badgeClass }, props.isAvailable ? "Available" : "Unavailable")
     )
 }
 
