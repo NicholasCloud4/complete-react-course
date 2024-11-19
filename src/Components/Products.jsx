bd12f8c8f3fc489bf8f2ffbf3b368f4447c0b061
Nicholas <91276870+NicholasCloud4@users.noreply.github.com>
2024-11-19 10:31:40 -0500

    Passing data via props

--- src/Components/Products.jsx
+++ src/Components/Products.jsx
@@ -1,12 +1,10 @@
 import React from "react";
 
 let productCount = 0;
-let imageUrl = require("../images/fresh-milk.jpg")
-let isAvailable = "Available";
+//let imageUrl = require("../images/fresh-milk.png")
+//let isAvailable = "Available";
 
-let badgeClass = "badge-margin-left-240 badge ";
-badgeClass += isAvailable === "Available" ? "badge bg-success" : "badge bg-danger"
-console.log(badgeClass)
+
 // let style = {
 //     padding: "0px 20px",
 //     fontSize: "12px"
@@ -16,25 +14,27 @@ function displayFormattedProductCount() {
     return productCount > 0 ? productCount : "ZERO";
 }
 
-function Products() {
+function Products(props) {
+    let badgeClass = "badge-margin-left-240 badge ";
+    badgeClass += props.isAvailable ? "badge bg-success" : "badge bg-danger"
     return (
         <>
             <ul className="list-group shadow">
                 <li className="list-group-item">
                     <div className="media align-items-lg-center flex-column flex-lg-row p-3 d-flex">
                         <div className="media-body order-2 order-lg-1">
-                            <h5 className="mt-0 font-weight-bold mb-2">Fresh Milk</h5>
-                            <p className="font-italic text-muted mb-0 small">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Suscipit fuga autem maiores necessitatibus.</p>
+                            <h5 className="mt-0 font-weight-bold mb-2">{props.name}</h5>
+                            <p className="font-italic text-muted mb-0 small">{props.description}</p>
                             <div className="d-flex align-items-center justify-content-start mt-1">
-                                <h6 className="font-weight-bold my-2" style={{ marginRight: "30px" }}>$120.00</h6>
+                                <h6 className="font-weight-bold my-2" style={{ marginRight: "30px" }}>{props.price}</h6>
                                 <button className="btn btn-primary">-</button>
                                 <span style={{ padding: "0px 20px", fontSize: "12px" }}>{displayFormattedProductCount()}</span>
                                 <button className="btn btn-primary ">+</button>
-                                <span className={badgeClass}>{isAvailable}</span>
+                                <span className={badgeClass}>{props.isAvailable ? "Available" : "Unavailable"}</span>
 
                             </div>
                         </div>
-                        <img src={imageUrl} alt="Generic placeholder image" width="200" className="ml-lg-5 order-1 order-lg-2" />
+                        <img src={props.imageUrl} alt="Generic placeholder image" width="200" className="ml-lg-5 order-1 order-lg-2" />
                     </div>
                 </li>
             </ul>
