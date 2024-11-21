e8d05634e409a7c5d6ebda6971147c2b497be5ca
Nicholas <91276870+NicholasCloud4@users.noreply.github.com>
2024-11-21 15:04:27 -0500

    Rendering Conditional Content

--- src/Components/ProductList/ProductList.jsx
+++ src/Components/ProductList/ProductList.jsx
@@ -6,23 +6,29 @@ import Products from "./Products";
 function ProductList(props) {
     //console.log(props.newProduct)
 
-    return (
-        <ul className="list-group shadow">
-            {props.newProductList.map((product) => {
-                return (
-                    <Products
-                        key={product?.pID}
-                        id={product?.pID}
-                        name={product?.pName}
-                        description={product?.desc}
-                        isAvailable={product?.isAvailable}
-                        imageUrl={product?.image}
-                        price={product?.price}
-                    />
-                )
-            })}
-        </ul>
-    )
+    if (props.newProductList.length === 0) {
+        return (
+            <h3>No Products Available</h3>
+        )
+    } else {
+        return (
+            <ul className="list-group shadow">
+                {props.newProductList.map((product) => {
+                    return (
+                        <Products
+                            key={product?.pID}
+                            id={product?.pID}
+                            name={product?.pName}
+                            description={product?.desc}
+                            isAvailable={product?.isAvailable}
+                            imageUrl={product?.image}
+                            price={product?.price}
+                        />
+                    )
+                })}
+            </ul>
+        )
+    }
 }
 
 export default ProductList
